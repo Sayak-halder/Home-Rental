8bba47e5a70945917d219a9e0be7d365dad099cf
SayakHalder0 <thedeadman894@gmail.com>
2024-06-23 21:26:12 +0530

    changes in listing details

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import CreateListing from "./pages/CreateListing";
-// import ListingDetails from "./pages/ListingDetails";
+import ListingDetails from "./pages/ListingDetails";
 // import TripList from "./pages/TripList";
 // import WishList from "./pages/WishList";
 // import PropertyList from "./pages/PropertyList";
@@ -21,7 +21,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          {/* <Route path="/properties/:listingId" element={<ListingDetails />} /> */}
+          <Route path="/properties/:listingId" element={<ListingDetails />} />
           {/* <Route path="/properties/category/:category" element={<CategoryPage />} /> */}
           {/* <Route path="/properties/search/:search" element={<SearchPage />} /> */}
           {/* <Route path="/:userId/trips" element={<TripList />} /> */}
